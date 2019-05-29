d35d298120a6f705048946f54d697a9c5b035da7
Jan Medved <jmedved@cisco.com>
2019-05-29 10:55:45 -0700

    Remove debug messages

--- msgDump.go
+++ msgDump.go
@@ -100,12 +100,7 @@ func dumpFields(b *bytes.Buffer) {
 	}
 
 	if mType != cmdRequest && mType != cmdResponse && mType != event {
-		fmt.Printf("returning, eventType %d\n", mType)
 		return
-	}
-
-	if mType == event {
-		dumpBytes(b)
 	}
 
 	inList := false
